30676741c798838dbc45b337c3846ffeda0a217c
GP2015 <adamcunningham2015@hotmail.com>
2026-01-14 16:47:27 +0000

    Added config file

---
+++ include/config.hpp
@@ -0,0 +1,6 @@
+constexpr bool USE_MOTORS = true;
+constexpr bool USE_ACTUATORS = true;
+constexpr bool USE_IMU = true;
+
+constexpr bool USE_SERIAL_MONITOR = true;
+constexpr bool PRINT_ROTATION = true;
