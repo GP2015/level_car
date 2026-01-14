8b732d7be396d82e0245f873cd406fbab1358edf
GP2015 <adamcunningham2015@hotmail.com>
2026-01-14 18:05:06 +0000

    Replaced HIGH/LOW with 1/0

--- src/motor.cpp
+++ src/motor.cpp
@@ -21,8 +21,8 @@ void init_motors()
 
 void update_motors(bool l_dir, bool r_dir, float l_pow, float r_pow)
 {
-    digitalWrite(L_DRIVE_DIR_PIN, l_dir ? HIGH : LOW);
-    digitalWrite(R_DRIVE_DIR_PIN, r_dir ? HIGH : LOW);
+    digitalWrite(L_DRIVE_DIR_PIN, l_dir ? 1 : 0);
+    digitalWrite(R_DRIVE_DIR_PIN, r_dir ? 1 : 0);
 
     analogWrite(L_DRIVE_PWM_PIN, l_pow * MAX_MOTOR_POWER);
     analogWrite(R_DRIVE_PWM_PIN, r_pow * MAX_MOTOR_POWER);
